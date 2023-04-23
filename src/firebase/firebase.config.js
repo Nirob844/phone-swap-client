4ba7603cdbfc0dbebaa3878210de6f05452ec8dd
Nirob Hasan <nirob427417@gmail.com>
2023-04-23 11:14:56 +0600

    header & footer

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAmfc7t2hz-ZjPgz3iA26AfwdfuAFVTbFc",
+    authDomain: "phone-swap-b62bd.firebaseapp.com",
+    projectId: "phone-swap-b62bd",
+    storageBucket: "phone-swap-b62bd.appspot.com",
+    messagingSenderId: "830590897390",
+    appId: "1:830590897390:web:bebaa078f077d09163b7ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
